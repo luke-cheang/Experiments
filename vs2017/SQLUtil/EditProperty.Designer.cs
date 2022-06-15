68ad07e6f6ddaa4adbc91d8e226c6a464635f58d
luke-cheang <luke_cheang@engsound.com.tw>
2022-06-15 14:41:52 +0800

    加入屬性編輯。

---
+++ vs2017/SQLUtil/EditProperty.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace SQLUtil
+{
+    partial class EditProperty
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbKey = new System.Windows.Forms.Label();
+            this.txtValue = new System.Windows.Forms.TextBox();
+            this.btnOK = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbKey
+            // 
+            this.lbKey.AutoSize = true;
+            this.lbKey.Location = new System.Drawing.Point(11, 11);
+            this.lbKey.Name = "lbKey";
+            this.lbKey.Size = new System.Drawing.Size(27, 12);
+            this.lbKey.TabIndex = 0;
+            this.lbKey.Text = "Key:";
+            // 
+            // txtValue
+            // 
+            this.txtValue.Location = new System.Drawing.Point(11, 31);
+            this.txtValue.Name = "txtValue";
+            this.txtValue.Size = new System.Drawing.Size(300, 22);
+            this.txtValue.TabIndex = 1;
+            // 
+            // btnOK
+            // 
+            this.btnOK.Location = new System.Drawing.Point(11, 59);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(75, 23);
+            this.btnOK.TabIndex = 2;
+            this.btnOK.Text = "OK";
+            this.btnOK.UseVisualStyleBackColor = true;
+            this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(91, 59);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 3;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // EditProperty
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(339, 104);
+            this.ControlBox = false;
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnOK);
+            this.Controls.Add(this.txtValue);
+            this.Controls.Add(this.lbKey);
+            this.Name = "EditProperty";
+            this.Text = "EditProperty";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbKey;
+        private System.Windows.Forms.TextBox txtValue;
+        private System.Windows.Forms.Button btnOK;
+        private System.Windows.Forms.Button btnCancel;
+    }
+}
---
+++ vs2017/SQLUtil/EditProperty.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SQLUtil
+{
+    public partial class EditProperty : Form
+    {
+        private String propValue { get; set; }
+
+        private bool returnCode { get; set; }
+
+        public EditProperty()
+        {
+            InitializeComponent();
+            propValue = string.Empty;
+            returnCode = false;
+            Visible = false;
+        }
+
+        public bool Execute(String key, String inValue, out String outValue)
+        {
+            returnCode = false;
+            propValue = inValue;
+            lbKey.Text = key + ":";
+            txtValue.Text = inValue;
+            ShowDialog();
+            outValue = propValue;
+            return returnCode;
+        }   // Execute();
+
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            propValue = txtValue.Text.Trim();
+            returnCode = true;
+            Close();
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            returnCode = false;
+            Close();
+        }
+    }
+}
