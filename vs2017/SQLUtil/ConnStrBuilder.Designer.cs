5f98ee6032383f9699f435eb220d835648123a19
luke-cheang <luke_cheang@engsound.com.tw>
2022-06-17 21:14:29 +0800

    加入產生新連線字串時，把密碼分離產生。

--- vs2017/SQLUtil/ConnStrBuilder.Designer.cs
+++ vs2017/SQLUtil/ConnStrBuilder.Designer.cs
@@ -28,25 +28,34 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.lbConnStr = new System.Windows.Forms.Label();
+            this.lbOrgConnStr = new System.Windows.Forms.Label();
             this.txtConnStr = new System.Windows.Forms.TextBox();
             this.btnParseConnStr = new System.Windows.Forms.Button();
             this.lvConnStrKeys = new System.Windows.Forms.ListView();
+            this.colNo = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.colKey = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.colValue = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.txtConnStrResult = new System.Windows.Forms.TextBox();
             this.btnGenerate = new System.Windows.Forms.Button();
-            this.colNo = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.txtPlainPassword = new System.Windows.Forms.TextBox();
+            this.txtBase64Password = new System.Windows.Forms.TextBox();
+            this.lbOutConnStr = new System.Windows.Forms.Label();
+            this.lbPlainPassword = new System.Windows.Forms.Label();
+            this.lbBase64Password = new System.Windows.Forms.Label();
+            this.chkSeparatePassword = new System.Windows.Forms.CheckBox();
+            this.btnTestConnection = new System.Windows.Forms.Button();
+            this.lbConnectionResult = new System.Windows.Forms.Label();
+            this.txtConnectionResult = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
-            // lbConnStr
+            // lbOrgConnStr
             // 
-            this.lbConnStr.AutoSize = true;
-            this.lbConnStr.Location = new System.Drawing.Point(11, 9);
-            this.lbConnStr.Name = "lbConnStr";
-            this.lbConnStr.Size = new System.Drawing.Size(93, 12);
-            this.lbConnStr.TabIndex = 1;
-            this.lbConnStr.Text = "Connection String:";
+            this.lbOrgConnStr.AutoSize = true;
+            this.lbOrgConnStr.Location = new System.Drawing.Point(11, 9);
+            this.lbOrgConnStr.Name = "lbOrgConnStr";
+            this.lbOrgConnStr.Size = new System.Drawing.Size(134, 12);
+            this.lbOrgConnStr.TabIndex = 1;
+            this.lbOrgConnStr.Text = "Original Connection String:";
             // 
             // txtConnStr
             // 
@@ -60,7 +69,7 @@
             // 
             this.btnParseConnStr.Location = new System.Drawing.Point(11, 51);
             this.btnParseConnStr.Name = "btnParseConnStr";
-            this.btnParseConnStr.Size = new System.Drawing.Size(75, 23);
+            this.btnParseConnStr.Size = new System.Drawing.Size(100, 23);
             this.btnParseConnStr.TabIndex = 3;
             this.btnParseConnStr.Text = "Parse";
             this.btnParseConnStr.UseVisualStyleBackColor = true;
@@ -84,6 +93,10 @@
             this.lvConnStrKeys.View = System.Windows.Forms.View.Details;
             this.lvConnStrKeys.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.lvConnStrKeys_MouseDoubleClick);
             // 
+            // colNo
+            // 
+            this.colNo.Text = "No";
+            // 
             // colKey
             // 
             this.colKey.Text = "Key";
@@ -94,37 +107,123 @@
             // 
             // txtConnStrResult
             // 
-            this.txtConnStrResult.Location = new System.Drawing.Point(11, 401);
+            this.txtConnStrResult.Location = new System.Drawing.Point(11, 421);
             this.txtConnStrResult.Name = "txtConnStrResult";
             this.txtConnStrResult.ReadOnly = true;
-            this.txtConnStrResult.Size = new System.Drawing.Size(772, 22);
-            this.txtConnStrResult.TabIndex = 6;
+            this.txtConnStrResult.Size = new System.Drawing.Size(774, 22);
+            this.txtConnStrResult.TabIndex = 8;
+            this.txtConnStrResult.TextChanged += new System.EventHandler(this.txtConnStrResult_TextChanged);
             // 
             // btnGenerate
             // 
             this.btnGenerate.Location = new System.Drawing.Point(11, 375);
             this.btnGenerate.Name = "btnGenerate";
-            this.btnGenerate.Size = new System.Drawing.Size(75, 23);
+            this.btnGenerate.Size = new System.Drawing.Size(100, 23);
             this.btnGenerate.TabIndex = 5;
             this.btnGenerate.Text = "Generate";
             this.btnGenerate.UseVisualStyleBackColor = true;
             this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
             // 
-            // colNo
+            // txtPlainPassword
             // 
-            this.colNo.Text = "No";
+            this.txtPlainPassword.Location = new System.Drawing.Point(11, 467);
+            this.txtPlainPassword.Name = "txtPlainPassword";
+            this.txtPlainPassword.ReadOnly = true;
+            this.txtPlainPassword.Size = new System.Drawing.Size(384, 22);
+            this.txtPlainPassword.TabIndex = 10;
+            // 
+            // txtBase64Password
+            // 
+            this.txtBase64Password.Location = new System.Drawing.Point(401, 467);
+            this.txtBase64Password.Name = "txtBase64Password";
+            this.txtBase64Password.ReadOnly = true;
+            this.txtBase64Password.Size = new System.Drawing.Size(384, 22);
+            this.txtBase64Password.TabIndex = 12;
+            // 
+            // lbOutConnStr
+            // 
+            this.lbOutConnStr.AutoSize = true;
+            this.lbOutConnStr.Location = new System.Drawing.Point(11, 406);
+            this.lbOutConnStr.Name = "lbOutConnStr";
+            this.lbOutConnStr.Size = new System.Drawing.Size(128, 12);
+            this.lbOutConnStr.TabIndex = 7;
+            this.lbOutConnStr.Text = "Output Connection String:";
+            // 
+            // lbPlainPassword
+            // 
+            this.lbPlainPassword.AutoSize = true;
+            this.lbPlainPassword.Location = new System.Drawing.Point(11, 451);
+            this.lbPlainPassword.Name = "lbPlainPassword";
+            this.lbPlainPassword.Size = new System.Drawing.Size(77, 12);
+            this.lbPlainPassword.TabIndex = 9;
+            this.lbPlainPassword.Text = "Plain Password:";
+            // 
+            // lbBase64Password
+            // 
+            this.lbBase64Password.AutoSize = true;
+            this.lbBase64Password.Location = new System.Drawing.Point(401, 451);
+            this.lbBase64Password.Name = "lbBase64Password";
+            this.lbBase64Password.Size = new System.Drawing.Size(88, 12);
+            this.lbBase64Password.TabIndex = 11;
+            this.lbBase64Password.Text = "Base64 Password:";
+            // 
+            // chkSeparatePassword
+            // 
+            this.chkSeparatePassword.AutoSize = true;
+            this.chkSeparatePassword.Location = new System.Drawing.Point(131, 379);
+            this.chkSeparatePassword.Name = "chkSeparatePassword";
+            this.chkSeparatePassword.Size = new System.Drawing.Size(109, 16);
+            this.chkSeparatePassword.TabIndex = 6;
+            this.chkSeparatePassword.Text = "Separate Password";
+            this.chkSeparatePassword.UseVisualStyleBackColor = true;
+            // 
+            // btnTestConnection
+            // 
+            this.btnTestConnection.Location = new System.Drawing.Point(11, 501);
+            this.btnTestConnection.Name = "btnTestConnection";
+            this.btnTestConnection.Size = new System.Drawing.Size(100, 23);
+            this.btnTestConnection.TabIndex = 13;
+            this.btnTestConnection.Text = "Test Connection";
+            this.btnTestConnection.UseVisualStyleBackColor = true;
+            this.btnTestConnection.Click += new System.EventHandler(this.btnTestConnection_Click);
+            // 
+            // lbConnectionResult
+            // 
+            this.lbConnectionResult.AutoSize = true;
+            this.lbConnectionResult.Location = new System.Drawing.Point(115, 506);
+            this.lbConnectionResult.Name = "lbConnectionResult";
+            this.lbConnectionResult.Size = new System.Drawing.Size(59, 12);
+            this.lbConnectionResult.TabIndex = 14;
+            this.lbConnectionResult.Text = "Test Result:";
+            // 
+            // txtConnectionResult
+            // 
+            this.txtConnectionResult.Location = new System.Drawing.Point(181, 502);
+            this.txtConnectionResult.Name = "txtConnectionResult";
+            this.txtConnectionResult.ReadOnly = true;
+            this.txtConnectionResult.Size = new System.Drawing.Size(604, 22);
+            this.txtConnectionResult.TabIndex = 15;
             // 
             // ConnStrBuilder
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 456);
+            this.ClientSize = new System.Drawing.Size(800, 541);
+            this.Controls.Add(this.txtConnectionResult);
+            this.Controls.Add(this.lbConnectionResult);
+            this.Controls.Add(this.btnTestConnection);
+            this.Controls.Add(this.chkSeparatePassword);
+            this.Controls.Add(this.lbBase64Password);
+            this.Controls.Add(this.lbPlainPassword);
+            this.Controls.Add(this.lbOutConnStr);
+            this.Controls.Add(this.txtBase64Password);
+            this.Controls.Add(this.txtPlainPassword);
             this.Controls.Add(this.btnGenerate);
             this.Controls.Add(this.txtConnStrResult);
             this.Controls.Add(this.lvConnStrKeys);
             this.Controls.Add(this.btnParseConnStr);
             this.Controls.Add(this.txtConnStr);
-            this.Controls.Add(this.lbConnStr);
+            this.Controls.Add(this.lbOrgConnStr);
             this.Name = "ConnStrBuilder";
             this.Text = "ConnStrBuilder";
             this.Load += new System.EventHandler(this.ConnStrBuilder_Load);
@@ -135,7 +234,7 @@
 
         #endregion
 
-        private System.Windows.Forms.Label lbConnStr;
+        private System.Windows.Forms.Label lbOrgConnStr;
         private System.Windows.Forms.TextBox txtConnStr;
         private System.Windows.Forms.Button btnParseConnStr;
         private System.Windows.Forms.ListView lvConnStrKeys;
@@ -144,6 +243,15 @@
         private System.Windows.Forms.TextBox txtConnStrResult;
         private System.Windows.Forms.Button btnGenerate;
         private System.Windows.Forms.ColumnHeader colNo;
+        private System.Windows.Forms.TextBox txtPlainPassword;
+        private System.Windows.Forms.TextBox txtBase64Password;
+        private System.Windows.Forms.Label lbOutConnStr;
+        private System.Windows.Forms.Label lbPlainPassword;
+        private System.Windows.Forms.Label lbBase64Password;
+        private System.Windows.Forms.CheckBox chkSeparatePassword;
+        private System.Windows.Forms.Button btnTestConnection;
+        private System.Windows.Forms.Label lbConnectionResult;
+        private System.Windows.Forms.TextBox txtConnectionResult;
     }
 }
 
--- vs2017/SQLUtil/ConnStrBuilder.cs
+++ vs2017/SQLUtil/ConnStrBuilder.cs
@@ -18,7 +18,7 @@ namespace SQLUtil
         private SqlConnectionStringBuilder _sqlConnStrBuilder;
 
         /// <summary>
-        /// 
+        /// Default constructor of ConnStrBuilder.
         /// </summary>
         public ConnStrBuilder()
         {
@@ -28,7 +28,7 @@ namespace SQLUtil
         }   // ConnStrBuilder();
 
         /// <summary>
-        /// 
+        /// 把_sqlConnStrBuilder中的每一個Property內容都顯示在lvConnStrKeys中。
         /// </summary>
         private void Refresh_BuilderProperties()
         {
@@ -41,16 +41,26 @@ namespace SQLUtil
                 foreach (String key in _sqlConnStrBuilder.Keys)
                 {
                     Debug.WriteLine("_sqlConnStrBuilder[" + key + "]=[" + _sqlConnStrBuilder[key] + "]");
-                    ListViewItem item = new ListViewItem(i.ToString());
-                    item.SubItems.Add(key);
-                    item.SubItems.Add(_sqlConnStrBuilder[key].ToString());
+                    ListViewItem item = new ListViewItem(i.ToString());     // 第1欄是第幾個(從1開始)
+                    item.SubItems.Add(key);                                 // 第2欄是key (Property名稱)
+                    item.SubItems.Add(_sqlConnStrBuilder[key].ToString());  // 第3欄是value
                     lvConnStrKeys.Items.Add(item);
                     i++;
                 }
             }
             lvConnStrKeys.AutoResizeColumns(ColumnHeaderAutoResizeStyle.ColumnContent);
-            txtConnStrResult.Clear();
         }   // Refresh_BuilderProperties();
+
+        /// <summary>
+        /// 清除各輸出字串的內容。
+        /// </summary>
+        private void Clear_OutputStrings()
+        {
+            txtConnStrResult.Clear();
+            txtPlainPassword.Clear();
+            txtBase64Password.Clear();
+            txtConnectionResult.Clear();
+        }   // Clear_OutputStrings();
 
         /// <summary>
         /// Event handler for Load event of ConnStrBuilder.
@@ -62,6 +72,7 @@ namespace SQLUtil
             String connStr = ConfigurationManager.AppSettings["ConnStr"];
 
             btnParseConnStr.Enabled = false;
+            btnTestConnection.Enabled = false;
             if (!String.IsNullOrWhiteSpace(connStr))
                 txtConnStr.Text = connStr.Trim();
         }   // ConnStrBuilder_Load();
@@ -80,10 +91,13 @@ namespace SQLUtil
         }   // txtConnStr_TextChanged();
 
         /// <summary>
-        /// 
+        /// Event handler for Click event of btnParseConnStr.
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
+        /// <remarks>
+        /// 用_sqlConnStrBuilder來解析連線字串，再顯示解析結果(_sqlConnStrBuilder的Properties)。
+        /// </remarks>
         private void btnParseConnStr_Click(object sender, EventArgs e)
         {
             String s = String.Empty;
@@ -92,6 +106,7 @@ namespace SQLUtil
             {
                 s = txtConnStr.Text.Trim();
                 Debug.WriteLine("txtConnStr.Text=[" + s + "]");
+                Clear_OutputStrings();
                 _sqlConnStrBuilder.Clear();
                 _sqlConnStrBuilder.ConnectionString = s;
                 Refresh_BuilderProperties();
@@ -104,22 +119,44 @@ namespace SQLUtil
         }   // btnParseConnStr_Click();
 
         /// <summary>
-        /// 
+        /// Event handler for Click event of btnGenerate.
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
+        /// <remarks>
+        /// 用_sqlConnStrBuilder來產生新的連線字串。
+        /// </remarks>
         private void btnGenerate_Click(object sender, EventArgs e)
         {
             try
             {
-                txtConnStrResult.Text = _sqlConnStrBuilder.ConnectionString;
+                Clear_OutputStrings();
+                if (chkSeparatePassword.Checked)
+                {
+                    // 要把密碼分離出來。
+                    String pswd = _sqlConnStrBuilder.Password;
+                    // 先產生沒有密碼的連線字串。
+                    _sqlConnStrBuilder.Password = String.Empty;
+                    txtConnStrResult.Text = _sqlConnStrBuilder.ConnectionString;
+                    // 產生明碼的密碼及換成Base64的密碼。
+                    txtPlainPassword.Text = pswd;
+                    byte[] b64pswd = Encoding.ASCII.GetBytes(pswd);
+                    txtBase64Password.Text = Convert.ToBase64String(b64pswd);
+                    // 把密碼放回ConnectionStringBuilder。
+                    _sqlConnStrBuilder.Password = pswd;
+                }
+                else
+                {
+                    // 不用把密碼分離出來，直接產生連線字串。
+                    txtConnStrResult.Text = _sqlConnStrBuilder.ConnectionString;
+                }
             }
             catch (Exception ex)
             {
                 string msg = Common.GetExceptionString(ex);
                 MessageBox.Show(msg, "Generate ConnStr Exception", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
-        }
+        }   // btnGenerate_Click();
 
         /// <summary>
         /// Event handler for MouseDoubleClick event of lvConnStrKeys.
@@ -156,6 +193,33 @@ namespace SQLUtil
                 string msg = Common.GetExceptionString(ex);
                 MessageBox.Show(msg, "Edit ConnStr Exception", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
+        }
+
+        /// <summary>
+        /// Event handler for TextChanged event of txtConnStrResult.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void txtConnStrResult_TextChanged(object sender, EventArgs e)
+        {
+            if (!String.IsNullOrWhiteSpace(txtConnStrResult.Text))
+                btnTestConnection.Enabled = true;
+            else
+                btnTestConnection.Enabled = false;
+        }   // txtConnStrResult_TextChanged();
+
+        /// <summary>
+        /// Event handler for Click event of btnTestConnection.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        /// <remarks>
+        /// 測試產生的連線字串是否可以連上資料庫。
+        /// </remarks>
+        private void btnTestConnection_Click(object sender, EventArgs e)
+        {
+            txtConnectionResult.Clear();
+
         }
 
     }   // ConnStrBuilder
