6545936c1027c57e549fcc8ef7845238b2455f92
luke-cheang <luke_cheang@engsound.com.tw>
2023-08-29 09:36:29 +0800

    Try Interlocked

---
+++ vs2017/Interlocked/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Interlocking
+{
+    static class Program
+    {
+        static void WorkerThread()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                int mySeq = UniqueSequence.GetNextSequence();
+                Console.WriteLine("mySeq = " + mySeq.ToString());
+                Thread.Sleep(10);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Thread[] threads = new Thread[10];
+            for (int i = 0; i < 10; i++)
+            {
+                threads[i] = new Thread(WorkerThread);
+                threads[i].Start();
+                Thread.Sleep(50);
+            }
+            Thread.Sleep(1000);
+            for (int i = 0; i < threads.Length; i++)
+                threads[i].Join();
+        }
+    }
+}
---
+++ vs2017/Interlocked/UniqueSequence.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Interlocking
+{
+    public static class UniqueSequence
+    {
+        private static int _SeqNo = 0;
+
+        public static int GetNextSequence()
+        {
+            return Interlocked.Increment(ref _SeqNo) % 100;
+        }   
+    }   // UniqueSequence
+}
